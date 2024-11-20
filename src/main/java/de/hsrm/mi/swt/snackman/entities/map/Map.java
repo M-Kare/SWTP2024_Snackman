c41923772cbf4c9c715a1fcd90f85df569785feb
VivienWeber <vivien_weber@gmx.net>
2024-11-20 08:44:40 +0100

    refactor: add javadoc in Map class

--- src/main/java/de/hsrm/mi/swt/snackman/entities/map/Map.java
+++ src/main/java/de/hsrm/mi/swt/snackman/entities/map/Map.java
@@ -1,9 +1,18 @@
 package de.hsrm.mi.swt.snackman.entities.map;
 
+/**
+ * Represents a game map
+ */
 public class Map {
 
+    // representing map data with [x][z] coordinates
     private char[][] mapData;
 
+    /**
+     * Constructs a new Map with the given map data
+     *
+     * @param mapData A char array representing the initial state of the map
+     */
     public Map(char[][] mapData) {
         this.mapData = mapData;
     }
