892d5158239ab96353f4e292304d4c94a2f25c91
VivienWeber <vivien_weber@gmx.net>
2024-11-20 09:14:38 +0100

    refactor: add javadoc in TEXTURE

--- src/main/java/de/hsrm/mi/swt/snackman/entities/map/TEXTURE.java
+++ src/main/java/de/hsrm/mi/swt/snackman/entities/map/TEXTURE.java
@@ -1,5 +1,9 @@
 package de.hsrm.mi.swt.snackman.entities.map;
 
+/**
+ * Represents the different textures that can be applied to map objects
+ * This enum defines the available texture types for various elements in the game map
+ */
 public enum TEXTURE {
     WALL,
     FLOOR
