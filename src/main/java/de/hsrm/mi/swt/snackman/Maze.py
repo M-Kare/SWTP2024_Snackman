04859c6b33ed582033aa3ccbefc819c2d001f27d
Verena <Verena.kipp@student.hs-rm.de>
2024-11-20 12:05:14 +0100

    feature: snacks and spawnpoints

--- src/main/java/de/hsrm/mi/swt/snackman/Maze.py
+++ src/main/java/de/hsrm/mi/swt/snackman/Maze.py
@@ -1,9 +1,63 @@
 import random
 
-def erstelleLabyrinth(width, height):
-    laby = [["#" for i in range(width)] for i in range(height)] #erstmal alles #
+def searchFreeFieldBenath(maze, x, y):
+
+    if maze[x + 1][y + 1] != '#':
+        return (x + 1, y + 1)
+    elif maze[x][y+1] != '#':
+        return (x, y+1)
+    elif maze[x + 1][y] != '#':
+        return (x + 1, y)
+    elif maze[x - 1][y] != '#':
+        return (x - 1, y)
+    elif maze[x][y - 1] != '#':
+        return (x,y - 1)
+    
+    return None
+
+def generateSpawnGhost(maze):
+    for i in range(5):
+        randome1 = random.randint(1, len(maze) - 2)
+        randome2 = random.randint(1, len(maze) - 2)
+        print(randome1, " ", randome2)
+        if maze[randome1][randome2] != '#':
+            maze[randome1][randome2] = 'G'
+        else:
+            randome1, randome2 = searchFreeFieldBenath(maze, randome1, randome2)
+            maze[randome1][randome2] = 'G'
+
+    return maze
+
+def generateSpawnChicken(maze):
+    for i in range(10):
+        randome1 = random.randint(1, len(maze) - 2)
+        randome2 = random.randint(1, len(maze) - 2)
+        if maze[randome1][randome2] != '#' or maze[randome1][randome2] != 'G' or maze[randome1][randome2] != 'S':
+            maze[randome1][randome2] = 'C'
+        else:
+            randome1, randome2 = searchFreeFieldBenath(maze, randome1, randome2)
+
+    return maze
+
+def generateFreeCenter(maze):
+    for y in range(len(maze)):
+        for x in range(len(maze[y])):
+            if(width/3 < x < 2*width/3 and height/3 < y <(2*height/3)):
+                maze[y][x] = " "
+    
+    return maze
+
+def generateSpawnSnackman(maze):
+    center = (int) (len(maze)/2)
+    maze[center][center] = 'S'
+
+    return maze
+
+
+def generateLabyrinth(width, height):
+    maze = [["#" for i in range(width)] for i in range(height)] #erstmal alles #
     stack = [(1, 1)]
-    laby[1][1] = " "
+    maze[1][1] = " "
 
     while stack: #läuft so lange, wie es Elemente im Stack gibt
         x, y = stack[-1]
@@ -12,28 +66,37 @@ def erstelleLabyrinth(width, height):
 
         for rx, ry in richtungen:
             aktx, akty = x + rx, y + ry
-            if 1 <= aktx < width - 1 and 1 <= akty < height -1 and laby[akty][aktx] == "#":
-                laby[akty][aktx] = " "
-                laby[y + ry//2][x + rx//2] = " "
+            if 1 <= aktx < width - 1 and 1 <= akty < height -1 and maze[akty][aktx] == "#":
+                randomeNumber = random.randint(1, 10) #Snacks werden im verhältnis 1 zu 10 gespawnt
+                if(randomeNumber == 1):
+                    maze[akty][aktx] == "o"
+                    maze[y + ry//2][x + rx//2] = "o"
+                else:
+                    maze[akty][aktx] = " "
+                    maze[y + ry//2][x + rx//2] = " "
                 stack.append((aktx, akty))
                 break
-        else:#else einer for schleife wird ausgeführt, wenn Schleife nicht curch break verlassen wurde
+        else:#else einer for schleife wird ausgeführt, wenn Schleife nicht durch break verlassen wurde
             stack.pop()
+        
+    maze = generateSpawnGhost(maze)
 
-    for y in range(len(laby)):
-        for x in range(len(laby[y])):
-            if(width/3 < x < 2*width/3 and height/3 < y <(2*height/3)):
-                laby[y][x] = " "
+    maze = generateSpawnChicken(maze)
 
-    return laby
+    maze = generateFreeCenter(maze)
 
-def speicherInFile(laby, filename="Maze.txt"):
+    maze = generateFreeCenter(maze)
+
+    maze = generateSpawnSnackman(maze)
+
+    return maze
+
+def saveFile(maze, filename="Maze.txt"):
     with open(filename, "w") as file:
-        for row in laby:
+        for row in maze:
             file.write("".join(row) + "\n")
 
 width, height = 99, 99
-laby = erstelleLabyrinth(width, height)
-#laby = Labyrinth(width, height)
-speicherInFile(laby)
+maze = generateLabyrinth(width, height)
+saveFile(maze)
 print("Labyrinth fertig")
