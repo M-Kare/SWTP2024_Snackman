caf0c71a4bab2032a2c426fc543ae4de3dac53a8
VivienWeber <vivien_weber@gmx.net>
2024-11-20 10:02:57 +0100

    refactor: code refactoring

--- src/main/java/de/hsrm/mi/swt/snackman/api/MapAPIController.java
+++ src/main/java/de/hsrm/mi/swt/snackman/api/MapAPIController.java
@@ -1,6 +1,5 @@
 package de.hsrm.mi.swt.snackman.api;
 
-import de.hsrm.mi.swt.snackman.entities.map.Wall;
 import de.hsrm.mi.swt.snackman.services.MapService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
+// enable Cross-Origin Resource Sharing (CORS) for requests coming from the specified origin
 @CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api")
 public class MapAPIController {
