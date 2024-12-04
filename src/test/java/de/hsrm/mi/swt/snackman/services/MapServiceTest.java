5017e493f48f04aaf9a0686406110285b91daff5
Artur Konkel <Artur.Konkel@student.hs-rm.de>
2024-12-04 17:41:32 +0100

    MERGE - #24 Combine test cases

--- src/test/java/de/hsrm/mi/swt/snackman/services/MapServiceTest.java
+++ src/test/java/de/hsrm/mi/swt/snackman/services/MapServiceTest.java
@@ -1,18 +1,23 @@
 package de.hsrm.mi.swt.snackman.services;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 import de.hsrm.mi.swt.snackman.entities.map.GameMap;
 import de.hsrm.mi.swt.snackman.entities.map.Square;
 import de.hsrm.mi.swt.snackman.entities.mapObject.MapObjectType;
 import de.hsrm.mi.swt.snackman.entities.mapObject.snack.Snack;
-import de.hsrm.mi.swt.snackman.entities.mobileObjects.eatingMobs.SnackMan;
+import org.junit.jupiter.api.Assertions;
 import de.hsrm.mi.swt.snackman.messaging.FrontendMessageService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,9 +33,32 @@ public class MapServiceTest {
 
     private MapService mapService;
 
+
     @BeforeEach
     public void setUp() {
         mapService = new MapService(frontendMessageService, readMazeService);
+    }
+
+    @Test
+    void newMazeGeneratedWhenNewInstanceOfMapService() {
+        List<String> mazeBeforeMapService = new LinkedList<String>();
+        List<String> mazeAfterMapService = new LinkedList<String>();
+
+        try {
+            mazeBeforeMapService = Files.readAllLines(Paths.get("./Maze.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mapService.generateNewMaze();
+
+        try {
+            mazeAfterMapService = Files.readAllLines(Paths.get("./Maze.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assertions.assertNotEquals(mazeAfterMapService, mazeBeforeMapService, "No new maze generated");
+
     }
 
     @Test
@@ -42,7 +70,7 @@ public class MapServiceTest {
         writer.write("####\n");
         writer.close();
 
-        mapService = new MapService(frontendMessageService, readMazeService,"test_maze.txt");
+        mapService = new MapService(frontendMessageService, readMazeService, "test_maze.txt");
 
         MapObjectType[][] expectedMap = {
                 {MapObjectType.WALL, MapObjectType.WALL, MapObjectType.WALL, MapObjectType.WALL}, // First row
