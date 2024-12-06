a29d5d183fe7beb49889ea2d103f20482ab5ac03
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2024-12-06 16:24:29 +0100

    refactor: adjust walking speed and Maze.txt

--- src/main/java/de/hsrm/mi/swt/snackman/configuration/GameConfig.java
+++ src/main/java/de/hsrm/mi/swt/snackman/configuration/GameConfig.java
@@ -8,5 +8,5 @@ public class GameConfig {
     // SNACKMAN
     public static final int SNACKMAN_GROUND_LEVEL = 2;
     public static final double SNACKMAN_RADIUS = 0.3;
-    public static final int SNACKMAN_SPEED = 5;
+    public static final int SNACKMAN_SPEED = 8;
 }
