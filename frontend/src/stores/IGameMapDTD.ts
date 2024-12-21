ad0106bfba8ee0c4474058b32cd6100c4a6de8d7
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2024-12-21 11:53:13 +0100

    fix: add IScriptGhostDTD and IScriptGhost into IGameMapDTD

--- frontend/src/stores/IGameMapDTD.ts
+++ frontend/src/stores/IGameMapDTD.ts
@@ -1,13 +1,15 @@
 import type {ISquare, ISquareDTD} from "@/stores/Square/ISquareDTD";
 import type {IChicken, IChickenDTD} from "@/stores/Chicken/IChickenDTD";
 import type {IGhost, IGhostDTD} from "@/stores/Ghost/IGhostDTD";
+import type {IScriptGhost, IScriptGhostDTD} from "@/stores/Ghost/IScriptGhostDTD";
 
 export interface IGameMapDTD {
   DEFAULT_WALL_HEIGHT: number,
   DEFAULT_SQUARE_SIDE_LENGTH: number,
   gameMap: Array<ISquareDTD>,
   chickens: IChickenDTD[],
-  ghosts: IGhostDTD[]
+  ghosts: IGhostDTD[],
+  scriptGhosts: IScriptGhostDTD[]
 }
 
 export enum MapObjectType {
@@ -19,5 +21,6 @@ export interface IGameMap {
   DEFAULT_SQUARE_SIDE_LENGTH: number,
   gameMap: Map<number, ISquare>,
   chickens: IChicken[],
-  ghosts: IGhost[]
+  ghosts: IGhost[],
+  scriptGhosts: IScriptGhost[]
 }
