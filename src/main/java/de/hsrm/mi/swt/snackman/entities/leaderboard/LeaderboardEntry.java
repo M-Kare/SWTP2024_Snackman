d21fe2bf14e880119c36146e7c89f46bfa7198be
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-02 14:20:11 +0100

    feature: leaderboard can be read in by the LeaderboardService + LeaderboardEntrys are comparable

--- src/main/java/de/hsrm/mi/swt/snackman/entities/leaderboard/LeaderboardEntry.java
+++ src/main/java/de/hsrm/mi/swt/snackman/entities/leaderboard/LeaderboardEntry.java
@@ -1,12 +1,14 @@
 package de.hsrm.mi.swt.snackman.entities.leaderboard;
 
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
-public class LeaderboardEntry {
+public class LeaderboardEntry implements Comparable<LeaderboardEntry> {
     private String name;
     private LocalTime duration;
     private LocalDate releaseDate;
@@ -17,4 +19,31 @@ public class LeaderboardEntry {
         this.releaseDate = releaseDate;
     }
 
+    public LeaderboardEntry(String name, String duration, String releaseDate) {
+        this.name = name;
+        this.duration = LocalTime.parse(duration);
+        this.releaseDate = LocalDate.parse(releaseDate, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    @Override
+    public int compareTo(@NotNull LeaderboardEntry o) {
+        int durationComparison = this.duration.compareTo(o.duration);
+        if (durationComparison != 0) {
+            return durationComparison;
+        }
+        int releaseComparison = this.releaseDate.compareTo(o.releaseDate);
+        if (releaseComparison != 0) {
+            return releaseComparison;
+        }
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return "LeaderboardEntry{" +
+                "name='" + name + '\'' +
+                ", duration=" + duration +
+                ", releaseDate=" + releaseDate +
+                '}';
+    }
 }
--- src/main/java/de/hsrm/mi/swt/snackman/services/LeaderboardService.java
+++ src/main/java/de/hsrm/mi/swt/snackman/services/LeaderboardService.java
@@ -7,7 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,12 +23,46 @@ import java.util.List;
 public class LeaderboardService {
     Logger log = LoggerFactory.getLogger(MapService.class);
     private FrontendMessageService frontendMessageService;
-    private String filePath;
-    private List<LeaderboardEntry> leaderboard = new ArrayList<>();
+    private final String filePath;
+    private final List<LeaderboardEntry> leaderboard = new ArrayList<>();
+    private final String CSV_LINE_SPLITTER = ";";
 
     @Autowired
     public LeaderboardService(FrontendMessageService frontendMessageService) {
         this.frontendMessageService = frontendMessageService;
         this.filePath = "leaderboard.txt";
+        List<String> lines = readInLeaderboard();
+        fillLeaderboard(lines);
+        log.info("Leaderboard loaded: {}", leaderboard);
+    }
+
+    private List<String> readInLeaderboard() {
+        List<String> lines = new ArrayList<>();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read the " + filePath + " file.", e);
+        }
+
+        if (lines.isEmpty()) {
+            throw new RuntimeException(filePath + " is empty.");
+        }
+        return lines;
+    }
+
+    private void fillLeaderboard(List<String> lines) {
+        for (String line : lines) {
+            String[] parts = line.split(CSV_LINE_SPLITTER);
+            if (parts.length != 3) throw new RuntimeException("Invalid CSV line: " + line + " at " + filePath + " file.");
+            addLeaderboardEntry(new LeaderboardEntry(parts[0], parts[1], parts[2]));
+        }
+    }
+
+    private void addLeaderboardEntry(LeaderboardEntry entry) {
+        this.leaderboard.add(entry);
+        Collections.sort(this.leaderboard);
     }
 }
