937913f1b6024ed0d6c966fdf5cc38af2d40cc15
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-04 15:17:04 +0100

    feature: add method for adding a new entry to the leaderboard

--- frontend/src/stores/Leaderboard/leaderboardStore.ts
+++ frontend/src/stores/Leaderboard/leaderboardStore.ts
@@ -12,78 +12,100 @@ import type {LeaderboardEntry} from "@/stores/Leaderboard/LeaderboardDTD";
  * updated in the store itself.
  */
 export const useLeaderboardStore = defineStore('leaderboard', () => {
-  let leaderboardStompClient: Client
+    let leaderboardStompClient: Client
 
-  const leaderboard = reactive({
-    leaderboardEntries: []
-  } as Leaderboard)
+    const leaderboard = reactive({
+        leaderboardEntries: []
+    } as Leaderboard)
 
-  async function initLeaderboardStore() {
-    try {
-      const response: LeaderboardDTD = await fetchLeaderboardDataFromBackend()
+    async function initLeaderboardStore() {
+        try {
+            const response: LeaderboardDTD = await fetchLeaderboardDataFromBackend()
 
-      leaderboard.leaderboardEntries = []
-      for (const entry of response.leaderboardEntries) {
-        leaderboard.leaderboardEntries.push(entry as LeaderboardEntry)
-      }
+            leaderboard.leaderboardEntries = []
+            for (const entry of response.leaderboardEntries) {
+                leaderboard.leaderboardEntries.push(entry as LeaderboardEntry)
+            }
 
-      console.log("Initialised leaderboard data {}", leaderboard)
-    } catch (reason) {
-      throw reason
+            console.log("Initialised leaderboard data {}", leaderboard)
+        } catch (reason) {
+            throw reason
+        }
     }
-  }
 
-  async function startLeaderboardUpdate() {
-    const protocol = window.location.protocol.replace('http', 'ws')
-    const wsurl = `${protocol}//${window.location.host}/stompbroker`
-    const DEST_SQUARE = '/topic/leaderboard'
+    async function startLeaderboardUpdate() {
+        const protocol = window.location.protocol.replace('http', 'ws')
+        const wsurl = `${protocol}//${window.location.host}/stompbroker`
+        const DEST_SQUARE = '/topic/leaderboard'
 
-    if (!leaderboardStompClient) {
-      leaderboardStompClient = new Client({brokerURL: wsurl})
+        if (!leaderboardStompClient) {
+            leaderboardStompClient = new Client({brokerURL: wsurl})
 
-      leaderboardStompClient.onWebSocketError = (event) => {
-        throw new Error('Websocket wit message: ' + event)
-      }
+            leaderboardStompClient.onWebSocketError = (event) => {
+                throw new Error('Websocket wit message: ' + event)
+            }
 
-      leaderboardStompClient.onStompError = (frameElement) => {
-        throw new Error('Stompclient with message: ' + frameElement)
-      }
+            leaderboardStompClient.onStompError = (frameElement) => {
+                throw new Error('Stompclient with message: ' + frameElement)
+            }
 
-      leaderboardStompClient.onConnect = (frameElement) => {
-        console.log('Stompclient connected')
+            leaderboardStompClient.onConnect = (frameElement) => {
+                console.log('Stompclient connected')
 
-        leaderboardStompClient.subscribe(DEST_SQUARE, async (message) => {
-          const change: IFrontendLeaderboardEntryMessageEvent = JSON.parse(message.body)
-          leaderboard.leaderboardEntries.push(change.leaderboardEntry as LeaderboardEntry)
-          sortLeaderboard()
+                leaderboardStompClient.subscribe(DEST_SQUARE, async (message) => {
+                    const change: IFrontendLeaderboardEntryMessageEvent = JSON.parse(message.body)
+                    leaderboard.leaderboardEntries.push(change.leaderboardEntry as LeaderboardEntry)
+                    sortLeaderboard()
+                })
+            }
+
+            leaderboardStompClient.onDisconnect = () => {
+                console.log('Stompclient disconnected.')
+            }
+
+            leaderboardStompClient.activate()
+        }
+    }
+
+    async function addNewLeaderboardEntry(data: LeaderboardEntry) {
+        const url = 'http://localhost:8080/new/entry'
+
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }
+
+        try {
+            const response = await fetch(url, requestOptions);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            console.log('Success:', result);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function sortLeaderboard() {
+        leaderboard.leaderboardEntries.sort((a, b) => {
+            const durationComparison = a.duration.localeCompare(b.duration);
+            if (durationComparison !== 0) {
+                return durationComparison;
+            }
+            const releaseComparison = a.releaseDate.localeCompare(b.releaseDate);
+            if (releaseComparison !== 0) {
+                return releaseComparison;
+            }
+            return a.name.localeCompare(b.name);
         })
-      }
-
-      leaderboardStompClient.onDisconnect = () => {
-        console.log('Stompclient disconnected.')
-      }
-
-      leaderboardStompClient.activate()
     }
-  }
 
-  function sortLeaderboard() {
-    leaderboard.leaderboardEntries.sort((a, b) => {
-      const durationComparison = a.duration.localeCompare(b.duration);
-      if (durationComparison !== 0) {
-        return durationComparison;
-      }
-      const releaseComparison = a.releaseDate.localeCompare(b.releaseDate);
-      if (releaseComparison !== 0) {
-        return releaseComparison;
-      }
-      return a.name.localeCompare(b.name);
-    })
-  }
-
-  return {
-    leaderboard: readonly(leaderboard as Leaderboard),
-    initLeaderboardStore,
-    startLeaderboardUpdate,
-  }
+    return {
+        leaderboard: readonly(leaderboard as Leaderboard),
+        initLeaderboardStore,
+        startLeaderboardUpdate,
+        addNewLeaderboardEntry
+    }
 })
