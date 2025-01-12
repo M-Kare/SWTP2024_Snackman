cbd865bb257958aa77e9a82fd2c4ab6a59d2a4ea
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-12 10:09:22 +0100

    feature: add logic for checking the winningCondition when a mob has moved

--- src/main/java/de/hsrm/mi/swt/snackman/controller/PlayerMovement/PlayerStompController.java
+++ src/main/java/de/hsrm/mi/swt/snackman/controller/PlayerMovement/PlayerStompController.java
@@ -1,5 +1,8 @@
 package de.hsrm.mi.swt.snackman.controller.PlayerMovement;
 
+import de.hsrm.mi.swt.snackman.configuration.GameConfig;
+import de.hsrm.mi.swt.snackman.entities.lobby.ROLE;
+import de.hsrm.mi.swt.snackman.entities.mobileObjects.Mob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -9,6 +12,8 @@ import de.hsrm.mi.swt.snackman.entities.lobby.Lobby;
 import de.hsrm.mi.swt.snackman.entities.mobileObjects.Ghost;
 import de.hsrm.mi.swt.snackman.entities.mobileObjects.eatingMobs.SnackMan;
 import de.hsrm.mi.swt.snackman.services.LobbyManagerService;
+
+import java.util.Arrays;
 
 @Controller
 public class PlayerStompController {
@@ -20,8 +25,8 @@ public class PlayerStompController {
     public void spreadPlayerUpdate(@DestinationVariable("lobbyId") String lobbyId, PlayerToBackendDTO player) {
         Lobby currentLobby = lobbyService.findLobbyByLobbyId(lobbyId);
         var playerMob = switch (currentLobby.getClientMobMap().get(player.playerId())) {
-            case SnackMan snackman -> updateSnackman(player, snackman);
-            case Ghost ghost -> ghost;
+            case SnackMan snackman -> updateSnackman(player, snackman, currentLobby);
+            case Ghost ghost -> updateGhost(currentLobby, ghost);
             default ->
                     throw new IllegalStateException("Unexpected value: " + currentLobby.getClientMobMap().get(player.playerId()));
         };
@@ -29,7 +34,26 @@ public class PlayerStompController {
         playerMob.move(player.forward(), player.backward(), player.left(), player.right(), player.delta());
     }
 
-    private SnackMan updateSnackman(PlayerToBackendDTO player, SnackMan snackman) {
+    private Ghost updateGhost (Lobby currentLobby, Ghost ghost){
+        SnackMan snackman = getSnackman(currentLobby);
+        checkWinningCondition(snackman, currentLobby);
+
+        return ghost;
+    }
+
+    public SnackMan getSnackman(Lobby currentLobby) {
+        return Arrays.stream(currentLobby.getGameMap().getGameMapSquares())
+                .flatMap(Arrays::stream)
+                .flatMap(square -> square.getMobs().stream())
+                .filter(mob -> mob instanceof SnackMan)
+                .map(mob -> (SnackMan) mob)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Kein Snackman gefunden"));
+    }
+
+
+
+    private SnackMan updateSnackman(PlayerToBackendDTO player, SnackMan snackman, Lobby currentLobby) {
         if (player.jump()) {
             if (player.doubleJump()) {
                 snackman.doubleJump();
@@ -40,6 +64,16 @@ public class PlayerStompController {
         snackman.updateJumpPosition(player.delta());
         snackman.setSprinting(player.sprinting());
 
+        checkWinningCondition(snackman, currentLobby);
+
         return snackman;
+    }
+
+    private void checkWinningCondition(SnackMan snackman, Lobby currentLobby) {
+        if (snackman.getKcal() < 0) {
+            currentLobby.endGame(ROLE.GHOST);
+        } else if (snackman.getKcal() >= GameConfig.SNACKMAN_MAX_CALORIES) {
+            currentLobby.endGame(ROLE.SNACKMAN);
+        }
     }
 }
