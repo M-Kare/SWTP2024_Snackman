55054b10a9d34bd0894512a195b410fce7ad6fe9
Vivien Weber <vivien.weber@student.hs-rm.de>
2025-01-12 22:17:33 +0100

    fix: add correct button design (white button with a minimum of border-radius, solid black border and slightly offset drop shadow -> box-shadow)

--- frontend/src/components/SmallNavButton.vue
+++ frontend/src/components/SmallNavButton.vue
@@ -1,23 +1,24 @@
 <template>
-    <button class="small-nav-button">
-        <slot>Nav</slot> <!-- slot = default, will be overwritten -->
-    </button>
+  <button class="small-nav-button">
+    <slot>Nav</slot>
+    <!-- slot = default, will be overwritten -->
+  </button>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script lang="ts" setup></script>
 
 <style scoped>
 .small-nav-button {
-    position: inherit;
-    font-size: 1.2rem;
-    width: auto;
-    padding: 1rem;
-    height: auto;
-    text-align: center;
-    cursor: pointer;
-    border: none;
-    border-radius: 0.3rem;
+  background-color: var(--background-for-text-color);
+  position: inherit;
+  font-size: 1.2rem;
+  width: auto;
+  padding: 1rem;
+  height: auto;
+  text-align: center;
+  cursor: pointer;
+  border: var(--primary-text-color) solid 4px;
+  border-radius: 0.2rem;
+  box-shadow: 4px 3px 0 var(--primary-text-color);
 }
 </style>
