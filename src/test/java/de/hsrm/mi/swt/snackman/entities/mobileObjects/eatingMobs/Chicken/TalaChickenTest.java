0407098d1e78eb4e6be8aadaf56a2c339a877b43
Verena <Verena.kipp@student.hs-rm.de>
2025-01-13 20:17:27 +0100

    fix: only one TalaChickenTest

--- src/test/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/eatingMobs/Chicken/TalaChickenTest.java
+++ src/test/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/eatingMobs/Chicken/TalaChickenTest.java
@@ -2,66 +2,204 @@ package de.hsrm.mi.swt.snackman.entities.mobileObjects.eatingMobs.Chicken;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+//TODO Talachicken cannot be tested, because not the right script is loaded. To fix that, we need to copy the
+// talachicken script into extension folder
 public class TalaChickenTest {
 
-    //TODO Talachicken cannot be tested, because not the right script is loaded. To fix that, we need to copy the
-    // talachicken script into extension folder
-/*
     @Test
-    public void talaChickenMovementTest(){
+    public void talaChickenGoesToEmptySpace(){
         Chicken chicken = new Chicken("TalaChickenMovementSkript");
 
-        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W", "L", "W", "L", "0");
-        List<String> result = chicken.executeMovementSkript(visibleEnvironment);
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "H", "W", "L",
+                                                    "L", "W", "W", "W", "L",
+                                                    "L", "W", "L", "W", "L");
 
-        int chosenDirectionIndex = Integer.parseInt(result.get(result.size() - 1));
+        int result = chicken.executeMovementSkript(visibleEnvironment);
 
-        assertEquals(" ", result.get(chosenDirectionIndex),
+        assertEquals( 0 , result,
                 "The Chicken should move to the empty space (' ') matching its new direction.");
     }
 
     @Test
-    public void testTalaCkickenFollowsSnackmanWithBigDistance(){
+    public void talaChickenGoesToEmptySpace1(){
         Chicken chicken = new Chicken("TalaChickenMovementSkript");
 
-        List<String> visibleEnvironment = List.of("W", "W", "W", "W", "L", "W", "W", "W", "W",
-                                                    "W", "L", "L", "L", "W", "L", "W", "L", "W",
-                                                    "W", "W", "L", "W", "W", "L", "L", "L", "W",
-                                                    "L", "L", "L", "L", "W", "L", "W", "W", "W",
-                                                    "W", "L", "W", "SM", "H", "L", "W", "L", "W", //H is Chicken
-                                                    "W", "L", "W", "L", "W", "L", "W", "L", "L",
-                                                    "W", "L", "W", "L", "W", "W", "W", "L", "W",
-                                                    "W", "W", "W", "L", "L", "W", "W", "W", "W",
-                                                    "W", "W", "W", "W", "L", "W", "W", "W", "W");
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "W", "L", "L",
+                                                    "L", "W", "H", "L", "L",
+                                                    "L", "W", "W", "W", "L",
+                                                    "L", "W", "L", "W", "L");
 
-        List<String> result = chicken.executeMovementSkript(visibleEnvironment);
+        int result = chicken.executeMovementSkript(visibleEnvironment);
 
-        boolean istFreierIndexVorhanden = result.contains(" ");
-        assertEquals(true, istFreierIndexVorhanden, "Empty element, but Snackman is not reachable : "+ result);
-
+        assertEquals( 1 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
     }
 
     @Test
-    public void testTalaChickenFollowsSnackmanButCannotReach(){
-    //weil z.B. Wäde im Weg sind
+    public void talaChickenGoesToEmptySpace2(){
         Chicken chicken = new Chicken("TalaChickenMovementSkript");
 
-        List<String> visibleEnvironment = List.of("W", "W", "W", "W", "W", "W", "W", "W", "W",
-                                                "W", "SM", "W", "L", "W", "L", "L", "L", "W",
-                                                "W", "W", "W", "L", "W", "L", "W", "L", "W",
-                                                "W", "L", "W", "L", "W", "L", "W", "W", "W",
-                                                "W", "L", "W", "L", "H", "L", "L", "L", "L",
-                                                "W", "L", "W", "L", "W", "L", "W", "L", "L",
-                                                "W", "L", "W", "L", "W", "L", "W", "L", "W",
-                                                "W", "L", "W", "L", "L", "L", "W", "L", "W",
-                                                "W", "L", "W", "L", "W", "W", "W", "L", "W");
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "W", "L", "L",
+                                                    "L", "W", "H", "W", "L",
+                                                    "L", "W", "L", "W", "L",
+                                                    "L", "W", "L", "W", "L");
 
-        List<String> result = chicken.executeMovementSkript(visibleEnvironment);
-        boolean istFreierIndexVorhanden = result.contains(" ");
-        assertEquals(true, istFreierIndexVorhanden, "Empty element, but Snackman is not reachable : "+ result);
+        int result = chicken.executeMovementSkript(visibleEnvironment);
 
+        assertEquals( 2 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
     }
 
- */
-    
+    @Test
+    public void talaChickenGoesToEmptySpace3(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "W", "L", "L",
+                                                    "L", "L", "H", "W", "L",
+                                                    "L", "W", "W", "W", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 3 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenGoesToSnackMan(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "SM", "L", "L",
+                                                    "L", "L", "H", "L", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 0 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenGoesToSnackMan2(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "L", "H", "SM", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 1 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenGoesToSnackMan3(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "L", "H", "L", "L",
+                                                    "L", "L", "SM", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 2 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenGoesToSnackMan4(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "SM", "H", "L", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 3 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenAvoidsGhost(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "G", "L", "L",
+                                                    "L", "L", "H", "L", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 2 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenAvoidsGhost2(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "L", "H", "G", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 3 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenAvoidsGhost3(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "L", "H", "L", "L",
+                                                    "L", "L", "G", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 0 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+
+    @Test
+    public void talaChickenAvoidsGhost4(){
+        Chicken chicken = new Chicken("TalaChickenMovementSkript");
+
+        List<String> visibleEnvironment = List.of("W", "W", "W", "L", "W",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "G", "H", "L", "L",
+                                                    "L", "L", "L", "L", "L",
+                                                    "L", "W", "L", "W", "L");
+
+        int result = chicken.executeMovementSkript(visibleEnvironment);
+
+        assertEquals( 1 , result,
+                "The Chicken should move to the empty space (' ') matching its new direction.");
+    }
+ 
 }
