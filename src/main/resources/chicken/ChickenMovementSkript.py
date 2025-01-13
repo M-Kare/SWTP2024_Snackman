bb6d1b2df5efbc93f1f8be8c464cffa99d236b6e
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-13 15:40:30 +0100

    fix: ChickenMovementSkript.py returning integers instead of a list

--- src/main/resources/chicken/ChickenMovementSkript.py
+++ src/main/resources/chicken/ChickenMovementSkript.py
@@ -174,8 +174,7 @@ def add_walking_direction(original_liste):
     """
     new_liste = [original_liste[0]] + original_liste[1:]
     first_empty_index = next((i for i, x in enumerate(new_liste) if x == " "), None)
-    result = new_liste + [first_empty_index]
-    return result
+    return first_empty_index
 
 
 def all_squares_have_snack(original_liste):
