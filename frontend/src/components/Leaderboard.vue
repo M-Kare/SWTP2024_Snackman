c5f1695aa534555c8ba84ca72b68ebad8be4bb4e
Vivien Weber <vivien.weber@student.hs-rm.de>
2025-01-13 18:49:10 +0100

    feat: add whole design for Leaderboard.vue

--- frontend/src/components/Leaderboard.vue
+++ frontend/src/components/Leaderboard.vue
@@ -1,34 +1,30 @@
 <template>
-  <div v-if="show" id="darken-background"></div>
-  <PopUp class="popup-box"
-         v-if="show"
-         @hidePopUp="$emit('close')">
-
-    <p class="info-heading"> Leaderboard </p>
-    <p class="info-text"> This is the current leaderboard: </p>
+  <div id="individual-outer-box-size" class="outer-box">
+    <h1 class="info-heading"> Leaderboard </h1>
     <div class="table-container">
       <table>
         <thead>
         <tr>
+          <td></td>
           <td>Name</td>
-          <td>Duration</td>
           <td>Date</td>
+          <td>Duration</td>
         </tr>
         </thead>
         <tbody>
-        <tr v-for="entry in leaderboardEntries">
+        <tr v-for="(entry, index) in leaderboardEntries">
+          <td>{{ index + 1 }}</td>
           <td>{{ entry.name }}</td>
-          <td>{{ entry.duration }}</td>
           <td>{{ entry.releaseDate }}</td>
+          <td>{{ entry.duration }}</td>
         </tr>
         </tbody>
       </table>
     </div>
-  </PopUp>
+  </div>
 </template>
 
-<script setup lang="ts">
-import PopUp from "@/components/PopUp.vue";
+<script lang="ts" setup>
 import {useLeaderboardStore} from "@/stores/Leaderboard/leaderboardStore";
 import {computed, onMounted} from "vue";
 
@@ -59,57 +55,94 @@ defineEmits(['close'])
 .info-heading {
   font-size: 3rem;
   font-weight: bold;
+  color: var(--background-for-text-color);
+  text-align: left;
 }
 
-.info-text {
-  font-size: 1.8rem;
-  padding: 1.2rem;
-}
-
-.info-text {
-  font-size: 1.8rem;
-  padding: 1.2rem;
-}
-
-#darken-background {
-  z-index: 1;
-  position: fixed;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  background: rgba(0, 0, 0, 50%);
-
-  transition: background 0.3s ease;
-}
-
-#popup-box {
-  z-index: 10;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: #ffffff;
-  border-radius: 0.3rem;
-  text-align: center;
-  padding: 2rem;
-  color: #000000;
-  max-width: 90vw;
-  max-height: 90vh;
-  width: auto;
-  height: auto;
+#individual-outer-box-size {
+  width: 70vw;
+  max-width: 1000px;
+  height: 40rem;
+  max-height: 50rem;
+  padding: 30px;
 }
 
 table {
   table-layout: fixed;
   width: 100%;
-  border-collapse: collapse;
+  border-collapse: separate;
+  border-spacing: 0 15px;
   overflow-x: auto;
 }
 
-th,
-td {
-  padding: 5px;
-  text-align: left;
+tr {
+  font-size: 1.2rem;
+}
+
+thead tr td {
+  background: transparent;
+  color: var(--background-for-text-color);
+  font-weight: bold;
+  border: none;
+  padding: 0 10px;
+}
+
+thead tr td {
+  background: transparent;
+  vertical-align: bottom;
+  color: var(--background-for-text-color);
+  font-weight: bold;
+  border: none;
+  padding: 5px 10px;
+  box-shadow: none;
+}
+
+thead tr td:first-child,
+thead tr td:last-child {
+  border: none;
+}
+
+thead tr td:not(:first-child):not(:last-child) {
+  border: none;
+}
+
+tbody tr td {
+  margin: 5px 0;
+  padding: 5px 10px;
+  background: var(--background-for-text-color);
+  box-shadow: 10px 8px 0 var(--primary-text-color);
+  color: var(--primary-text-color)
+}
+
+tr td:first-child,
+td:last-child {
+  border: 4px solid var(--primary-text-color);
+}
+
+tr td:first-child {
+  width: 7%;
+  text-align: right;
+  border-right: none;
+  border-top-left-radius: 0.1rem;
+  border-bottom-left-radius: 0.1rem;
+}
+
+tr td:last-child {
+  width: 15%;
+  text-align: center;
+  border-left: none;
+  border-top-right-radius: 0.1rem;
+  border-bottom-right-radius: 0.1rem;
+}
+
+tr td:nth-child(3) {
+  width: 20%;
+  text-align: center;
+}
+
+tr td:not(:first-child):not(:last-child) {
+  border-top: 4px solid var(--primary-text-color);
+  border-bottom: 4px solid var(--primary-text-color);
 }
 
 .table-container {
