92823ce8b39028ec03a3a67dfac34e20ca7502a5
Vivien Weber <vivien.weber@student.hs-rm.de>
2025-01-15 15:06:55 +0100

    fix: fix Chicken and ChickenTest because of merge dev into this branch

--- src/test/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/eatingMobs/Chicken/ChickenTest.java
+++ src/test/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/eatingMobs/Chicken/ChickenTest.java
@@ -1,29 +1,22 @@
 package de.hsrm.mi.swt.snackman.entities.mobileObjects.eatingMobs.Chicken;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Timer;
 import de.hsrm.mi.swt.snackman.SnackmanApplication;
 import de.hsrm.mi.swt.snackman.entities.map.GameMap;
 import de.hsrm.mi.swt.snackman.entities.map.Square;
 import de.hsrm.mi.swt.snackman.entities.mapObject.MapObjectType;
 import de.hsrm.mi.swt.snackman.entities.mapObject.snack.Snack;
 import de.hsrm.mi.swt.snackman.entities.mapObject.snack.SnackType;
-import de.hsrm.mi.swt.snackman.services.LobbyManagerService;
 import de.hsrm.mi.swt.snackman.services.MapService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.FileSystemUtils;
+
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Timer;
 
 @SpringBootTest
 class ChickenTest {
@@ -114,53 +107,6 @@ class ChickenTest {
         Assertions.assertNotSame(initialTimer, newTimer);
         Assertions.assertNotNull(newTimer);
     }
-
-    @ParameterizedTest
-    @CsvSource({
-            "true, false",
-            "false, false"
-    })
-    void testStartNewTimer_ScaredStateAffectsDelay(boolean initialScaredState, boolean expectedScaredState) throws InterruptedException {
-        Square square = new Square(MapObjectType.FLOOR, 0, 0);
-        Chicken chicken = new Chicken(square, gameMap);
-        chicken.setKcal(2800);
-
-        System.out.println("Initial scared state: " + initialScaredState);
-        chicken.setScared(initialScaredState);
-
-        long startTime = System.currentTimeMillis();
-        System.out.println("Start time: " + startTime);
-
-        chicken.startNewTimer();
-
-        // Wait for the egg to be laid (kcal becomes 0) or timeout after 70 seconds
-        long timeout = 70000; // 70 seconds
-        long elapsedTime = 0;
-        while (chicken.getKcal() > 0 && elapsedTime < timeout) {
-            Thread.sleep(100);
-            elapsedTime = System.currentTimeMillis() - startTime;
-        }
-
-        System.out.println("Elapsed time: " + elapsedTime + " ms");
-        System.out.println("Final kcal: " + chicken.getKcal());
-
-        if (initialScaredState) {
-            System.out.println("Checking scared condition...");
-            Assertions.assertTrue(elapsedTime >= 30000 && elapsedTime <= 70000,
-                    "Delay should be between 30 and 70 seconds when scared. Actual: " + elapsedTime + " ms");
-        } else {
-            System.out.println("Checking not scared condition...");
-            Assertions.assertTrue(elapsedTime >= 30000 && elapsedTime <= 60000,
-                    "Delay should be between 30 and 60 seconds when not scared. Actual: " + elapsedTime + " ms");
-        }
-
-        Assertions.assertEquals(0, chicken.getKcal(), "kcal should be 0 after laying an egg");
-
-        System.out.println("Final scared state: " + chicken.isScared());
-        Assertions.assertEquals(expectedScaredState, chicken.isScared(),
-                "Expected scared state: " + expectedScaredState + ", Actual: " + chicken.isScared());
-    }
-
 
     @Test
     void chickenGetsFatWhenComsumincSnacks() {
