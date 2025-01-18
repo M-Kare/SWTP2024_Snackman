d55dcd586e201480a0a475241270e06a30943aab
Jens Hornung <jens.hornung@student.hs-rm.de>
2025-01-18 22:22:34 +0100

    feat: added initial functionality of localization using vue-i18n

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,31 @@ import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en: {
+      message: {
+        hello: 'hello world'
+      }
+    },
+    ja: {
+      message: {
+        hello: 'こんにちは、世界'
+      }
+    }
+  }
+})
+
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(i18n)
   .mount('#app');
