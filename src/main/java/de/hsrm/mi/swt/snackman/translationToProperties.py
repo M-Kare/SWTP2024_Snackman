3907780e9207a74dc5504a96ede0cfdc546d5155
Jens Hornung <jens.hornung@student.hs-rm.de>
2025-01-18 16:14:40 +0100

    feat: finalized the py-script to create properties-files

--- src/main/java/de/hsrm/mi/swt/snackman/translationToProperties.py
+++ src/main/java/de/hsrm/mi/swt/snackman/translationToProperties.py
@@ -1,14 +1,15 @@
 
-def processBodyLines(bodyLines, languages, translationLists):
+def process_translation_entries(translation_entries, languages, language_dicts):
     '''
     Durchläuft jede Zeile mit Übersetzungen:
         1. entfernt Whitespace & Kommentarzeilen
-        2. zerlegt Zeile in einzelne Attribute
-        3.1 für jede Sprache werden die übersetzten Werte extrahiert
-        3.2 die übersetzten Werte, werden mit dem PropertyKey verbunden und danach der translationList der aktuellen Sprache hinzugefügt
+        2. zerlegt Zeile in key und values
+        3. zip() kombiniert Elemente aus languages & values mit gleichem Index in einem Tupel
+            - alle Tupel werden durchlaufen und in Variablen language & value aufgeteilt
+            - das Key-value-Pair wird formatiert und der Liste der entsprechenden Sprache angehängt
     '''
     
-    for line in bodyLines:
+    for line in translation_entries:
         
         # 1.
         line = line.replace("\n", "").strip()
@@ -16,20 +17,17 @@ def processBodyLines(bodyLines, languages, translationLists):
             continue
 
         # 2.
-        lineAttr = line.split(";")
-        propertyKey = lineAttr[0]
-        propertyValues = lineAttr[1:]
+        line_attr = line.split(";")
+        key = line_attr[0]
+        values = line_attr[1:]
 
-        # 3.1
-        for langIndex in range(len(languages)):
-            currLang = languages[langIndex]
-            currPropValue = propertyValues[langIndex]
-            #3.2
-            translationString = f"{propertyKey}={currPropValue}"
-            translationLists[currLang].append(translationString)
+        # 3
+        for language, value in zip(languages, values):
+            translation_string = f"{key}={value}"
+            language_dicts[language].append(translation_string)
 
 
-def writePropertyFile(language, translationLists):
+def write_property_file(language, translation_lists):
     '''
     Erstellt / überschreibt .properties-File:
         Key = property
@@ -37,49 +35,48 @@ def writePropertyFile(language, translationLists):
     '''
     
     with open(f"./src/main/resources/languages/{language}_translation.properties", "w") as propertyFile:
-        for line in translationLists:
+        for line in translation_lists:
             propertyFile.write(line + "\n")
 
 
-def createStandardProperties(languages):
+def create_standard_properties(languages):
     '''
-    Erstellt standardLanguage.properties anhand der Werte der 1. Sprache der csv
+    Erstellt standardLanguage.properties anhand der Werte der 1. Sprache der CSV-Datei
     -> kopiert Datei und speichert unter anderem Namen
     '''
     
-    standardLanguage = languages[0]
-    with open(f"./src/main/resources/languages/{standardLanguage}_translation.properties", "r") as sourceFile:
-        with open(f"./src/main/resources/languages/standardLanguage.properties", "w") as fallbackFile:
-            fallbackFile.write("# Fallback properties \n")
+    standard_lang = languages[0]
+    with open(f"./src/main/resources/languages/{standard_lang}_translation.properties", "r") as sourceFile:
+        with open("./src/main/resources/languages/standard_language.properties", "w") as fallbackFile:
+            fallbackFile.write("# Standard properties \n")
             fallbackFile.write(sourceFile.read())
 
 
 def main():
     # Datei einlesen
-    filePath = "src/main/resources/static/translations.csv"
-    with open(filePath, "r") as translations:
-        linesInCSV = translations.readlines()
+    file_path = "src/main/resources/static/translations.csv"
+    with open(file_path, "r") as translations:
+        content = translations.readlines()
 
     # Sprachen aus Header-Zeile extrahieren
-    headerLineAttr = linesInCSV[0].replace("\n", "").split(";")
-    languages = headerLineAttr[1:]
+    header_line_attr = content[0].replace("\n", "").split(";")
+    languages = header_line_attr[1:]
 
     # Dict, da Key-Value-Pairs verarbeitet werden
-    # Für jede Sprache wird leere Liste erstellt
-    translationLists = {}
-    for currLang in languages:
-        translationLists[currLang] = []
+    # Für jede Sprache wird leere Liste erstellt, die im nächsten Schritt gefüllt wird
+    language_dict = {}
+    for language in languages:
+        language_dict[language] = []
 
     # Zeilen mit Übersetzungen verarbeiten & somit die translationLists füllen
-    bodyLines = linesInCSV[1:]
-    processBodyLines(bodyLines, languages, translationLists)
+    translation_entries = content[1:]
+    process_translation_entries(translation_entries, languages, language_dict)
 
-    # .properties-Dateien erstellen
-    for language in translationLists:
-        writePropertyFile(language, translationLists[language])
+    # .properties-Dateien für jede Sprache erstellen
+    for language in language_dict:
+        write_property_file(language, language_dict[language])
 
-    # Fallback-Datei erstellen (1. Sprache der CSV = Standard)
-    createStandardProperties(languages)
-
+    # Fallback-Datei erstellen
+    create_standard_properties(languages)
 
 main()
