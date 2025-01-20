6559bf6a521c8e68132c3f323ee0a8add8691a6c
Vivien Weber <vivien.weber@student.hs-rm.de>
2025-01-20 23:45:12 +0100

    fix: fix chooseRole-layout

--- frontend/src/views/LobbyViews/ChooseRole.vue
+++ frontend/src/views/LobbyViews/ChooseRole.vue
@@ -2,62 +2,67 @@
   <MenuBackground></MenuBackground>
 
   <div class="outer-box">
-    <div class="inner-box">
-      <h1 class="title"> {{ $t('chooseRole.title') }} </h1>
+    <div id="inner-box">
+      <h1 class="title">{{ $t('chooseRole.title') }}</h1>
       <div class="character-grid">
         <div
           v-for="button in buttons"
           :key="button.id"
-          :class="{ 'selected': button.selected }"
-          :style="button.selected ? { opacity: 0.3, cursor: 'not-allowed' } : {}"
+          :class="{ selected: button.selected }"
+          :style="
+            button.selected ? { opacity: 0.3, cursor: 'not-allowed' } : {}
+          "
           class="character-item"
           @click="selectCharacter(button)"
         >
-          <div class="image-container"
-               :style="button.selected ? { pointerEvents: 'none' } : {}">
-            <img :src="button.image" :alt="button.name" class="character-image">
+          <div
+            :style="button.selected ? { pointerEvents: 'none' } : {}"
+            class="image-container"
+          >
+            <img
+              :alt="button.name"
+              :src="button.image"
+              class="character-image"
+            />
           </div>
           <p class="character-name">{{ $t(button.translation) }}</p>
         </div>
       </div>
-      <div id="button-box">
-        <SmallNavButton
-          v-if="isPlayerAdmin"
-          id="start-game-button"
-          class="small-nav-buttons"
-          @click="startGame"
-        >
-          {{ $t('button.startGame') }}
-        </SmallNavButton>
-      </div>
     </div>
+    <SmallNavButton
+      v-if="isPlayerAdmin"
+      id="start-game-button"
+      class="small-nav-buttons"
+      @click="startGame"
+    >
+      {{ $t('button.startGame') }}
+    </SmallNavButton>
 
     <PopUp v-if="showPopUp" class="popup-box" @hidePopUp="hidePopUp">
-      <p class="info-heading"> {{ $t('popup.cantStart.heading') }} </p>
+      <p class="info-heading">{{ $t('popup.cantStart.heading') }}</p>
       <p class="info-text">{{ infoText }}</p>
     </PopUp>
 
     <PopUp v-if="showRolePopup" class="popup-box" @hidePopUp="hidePopUp">
-      <p class="info-heading"> {{ $t('popup.cantStart.heading') }} </p>
+      <p class="info-heading">{{ $t('popup.cantStart.heading') }}</p>
       <p class="info-text">{{ infoText }}</p>
     </PopUp>
 
     <div v-if="darkenBackground" id="darken-background"></div>
-
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, ref, watchEffect} from 'vue';
-import MenuBackground from '@/components/MenuBackground.vue';
-import SmallNavButton from '@/components/SmallNavButton.vue';
-import {useRoute, useRouter} from "vue-router";
-import PopUp from "@/components/PopUp.vue";
-import type {Button} from "@/stores/Lobby/lobbiesstore"
-import {useLobbiesStore} from "@/stores/Lobby/lobbiesstore";
-import {useI18n} from 'vue-i18n';
+import {computed, onMounted, ref, watchEffect} from 'vue'
+import MenuBackground from '@/components/MenuBackground.vue'
+import SmallNavButton from '@/components/SmallNavButton.vue'
+import {useRoute, useRouter} from 'vue-router'
+import PopUp from '@/components/PopUp.vue'
+import type {Button} from '@/stores/Lobby/lobbiesstore'
+import {useLobbiesStore} from '@/stores/Lobby/lobbiesstore'
+import {useI18n} from 'vue-i18n'
 
-const {t} = useI18n();
+const {t} = useI18n()
 
 const route = useRoute()
 const router = useRouter()
@@ -76,9 +81,13 @@ const lobbyUrl = route.params.lobbyId
 const infoHeading = ref()
 
 const isPlayerAdmin = computed(() => {
-  const currentPlayer = lobbiesStore.lobbydata.currentPlayer;
-  const lobby = lobbiesStore.lobbydata.lobbies.find(l => l.lobbyId === lobbyId);
-  return currentPlayer && lobby && currentPlayer.playerId === lobby.adminClient.playerId;
+  const currentPlayer = lobbiesStore.lobbydata.currentPlayer
+  const lobby = lobbiesStore.lobbydata.lobbies.find(l => l.lobbyId === lobbyId)
+  return (
+    currentPlayer &&
+    lobby &&
+    currentPlayer.playerId === lobby.adminClient.playerId
+  )
 })
 
 /**
@@ -90,16 +99,15 @@ const isPlayerAdmin = computed(() => {
  * @throws {Error} If the player or lobby is not found.
  */
 
-const selectedCharacter = ref<Button | null>(null);
+const selectedCharacter = ref<Button | null>(null)
 
 const selectCharacter = async (button: Button) => {
-
   // already choosen Role
   if (button.selected) {
-    infoText.value = t('chooseRole.alreadyChosen');
-    showPopUp.value = true;
-    darkenBackground.value = true;
-    return;
+    infoText.value = t('chooseRole.alreadyChosen')
+    showPopUp.value = true
+    darkenBackground.value = true
+    return
   }
   // sende LobbyId, PlayerId,Rolle, ButtonId, Selected, ButtonId
   const payload = {
@@ -108,35 +116,34 @@ const selectCharacter = async (button: Button) => {
     role: button.name.toUpperCase(), // "SNACKMAN" oder "GHOST"
     buttonId: button.id,
     selected: button.selected,
-  };
+  }
   try {
     // bekommt statuscode Zurück 200 Ok , 409 Conflict , 400 Badrequest
     const response = await fetch('/api/lobbies/lobby/choose/role', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(payload)
-    });
+      body: JSON.stringify(payload),
+    })
 
     if (response.ok) {
-      selectedCharacter.value = button;
-      darkenBackground.value = true;
+      selectedCharacter.value = button
+      darkenBackground.value = true
     } else if (response.status === 409) {
-      infoText.value = t('chooseRole.alreadyChosen');
-      showPopUp.value = true;
-      darkenBackground.value = true;
+      infoText.value = t('chooseRole.alreadyChosen')
+      showPopUp.value = true
+      darkenBackground.value = true
     } else {
-      infoText.value = t('chooseRole.error.selection');
-      showPopUp.value = true;
-      darkenBackground.value = true;
+      infoText.value = t('chooseRole.error.selection')
+      showPopUp.value = true
+      darkenBackground.value = true
     }
-
   } catch (error) {
-    console.error("Error selecting character:", error);
-    infoText.value = t('chooseRole.error.connectionFailed');
-    showPopUp.value = true;
-    darkenBackground.value = true;
+    console.error('Error selecting character:', error)
+    infoText.value = t('chooseRole.error.connectionFailed')
+    showPopUp.value = true
+    darkenBackground.value = true
   }
 }
 
@@ -158,7 +165,9 @@ const startGame = async () => {
   const playerId = lobbiesStore.lobbydata.currentPlayer.playerId
   let snackmanCounter: number = 0
   let memberCounter: number = 0
-  const lobby = lobbiesStore.lobbydata.lobbies.find(lobby => lobby.lobbyId === lobbyId)
+  const lobby = lobbiesStore.lobbydata.lobbies.find(
+    lobby => lobby.lobbyId === lobbyId,
+  )
 
   if (!playerId || !lobby) {
     console.error('Player or Lobby not found')
@@ -169,7 +178,7 @@ const startGame = async () => {
       if (member.role === 'UNDEFINED') {
         showPopUp.value = true
         darkenBackground.value = true
-        infoText.value = t('chooseRole.error.everyPlayerNeedsRole');
+        infoText.value = t('chooseRole.error.everyPlayerNeedsRole')
         return
       } else if (member.role === 'SNACKMAN') {
         snackmanCounter++
@@ -181,8 +190,8 @@ const startGame = async () => {
 
     if (snackmanCounter === 1 && memberCounter === lobby.members.length) {
       await lobbiesStore.startGame(lobby.lobbyId)
-      buttons.forEach(button => button.selected = false)
-      buttons.forEach(button => button.selectedBy = '')
+      buttons.forEach(button => (button.selected = false))
+      buttons.forEach(button => (button.selectedBy = ''))
       selectedCharacter.value = null
 
       await router.push({
@@ -195,7 +204,7 @@ const startGame = async () => {
     } else {
       showPopUp.value = true
       darkenBackground.value = true
-      infoText.value = t('chooseRole.error.exactlyOneSnackMan');
+      infoText.value = t('chooseRole.error.exactlyOneSnackMan')
     }
   } else {
     showPopUp.value = true
@@ -210,13 +219,14 @@ watchEffect(() => {
       lobby => lobby.lobbyId === lobbyUrl,
     )
     if (updatedLobby && updatedLobby.gameStarted) {
-
       const currentPlayerId = lobbiesStore.lobbydata.currentPlayer.playerId
-      const currentPlayerInUpdatedLobby = updatedLobby.members.find(member => member.playerId == currentPlayerId)
+      const currentPlayerInUpdatedLobby = updatedLobby.members.find(
+        member => member.playerId == currentPlayerId,
+      )
 
       if (currentPlayerInUpdatedLobby) {
-        buttons.forEach(button => button.selected = false)
-        buttons.forEach(button => button.selectedBy = '')
+        buttons.forEach(button => (button.selected = false))
+        buttons.forEach(button => (button.selectedBy = ''))
         selectedCharacter.value = null
         router.push({
           name: 'GameView',
@@ -240,29 +250,29 @@ watchEffect(() => {
   color: var(--main-text-color);
 }
 
-#button-box {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  padding: 3% 0;
-}
-
 .outer-box {
   position: absolute;
-  top: 25%;
+  top: 15%;
   left: 50%;
   transform: translateX(-50%);
   width: 90%;
-  height: 60%;
+  height: 70%;
   background: rgba(255, 255, 255, 60%);
   border-radius: 0.5rem;
   padding: 0 20px;
 }
 
 .character-grid {
+  padding-top: 5%;
   display: grid;
   grid-template-columns: repeat(5, 5fr);
   grid-gap: 20px;
+}
+
+#inner-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 #confirm-button {
@@ -313,6 +323,12 @@ watchEffect(() => {
   font-weight: bold;
   color: var(--primary-text-color);
   margin: 0;
+}
+
+#start-game-button {
+  position: absolute;
+  bottom: 7%;
+  left: 45%;
 }
 
 @media (max-width: 1300px) {
