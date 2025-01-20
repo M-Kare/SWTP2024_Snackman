2e902ef886e3c1cd8dd7337dc0eabe779624989d
Mariusz Kondracki <mariusz.kondracki@student.hs-rm.de>
2025-01-20 10:23:33 +0100

    line fixed

--- src/main/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/Mob.java
+++ src/main/java/de/hsrm/mi/swt/snackman/entities/mobileObjects/Mob.java
@@ -173,7 +173,7 @@ public abstract class Mob {
 
         //RESPAWN AUSSERHALB DES LABYRINTHS
         if (gameMap.getGameMapSquares() != null) {
-            if((position.y < 1) || (calcMapIndexOfCoordinate(xNew) < 0 || calcMapIndexOfCoordinate(xNew) >= gameMap.getGameMapSquares().length) ||  calcMapIndexOfCoordinate(zNew) < 0 || calcMapIndexOfCoordinate(zNew) >= gameMap.getGameMapSquares()[0].length){
+            if((position.y < 2) || (calcMapIndexOfCoordinate(xNew) < 0 || calcMapIndexOfCoordinate(xNew) >= gameMap.getGameMapSquares().length) ||  calcMapIndexOfCoordinate(zNew) < 0 || calcMapIndexOfCoordinate(zNew) >= gameMap.getGameMapSquares()[0].length){
                 respawn();
                 return;
             }
