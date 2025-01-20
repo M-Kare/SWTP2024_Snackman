359589fc79d1c17b31150b942e7ce3da49287ac2
Artur Konkel <Artur.Konkel@student.hs-rm.de>
2025-01-20 19:59:31 +0100

    BUGFIX: Adjust came configs

--- src/main/java/de/hsrm/mi/swt/snackman/configuration/GameConfig.java
+++ src/main/java/de/hsrm/mi/swt/snackman/configuration/GameConfig.java
@@ -9,19 +9,21 @@ public class GameConfig {
     // SNACKMAN
     public static final int SNACKMAN_GROUND_LEVEL = 2;
     public static final double SNACKMAN_RADIUS = 0.3;
-    public static final double SNACKMAN_SPEED = 10.0;
+    public static final double SNACKMAN_SPEED = 6.5;
     public static final double SNACKMAN_SPRINT_MULTIPLIER = 1.5;
     public static final int SNACKMAN_MAX_CALORIES = 20000;
 
     //SNACKMAN JUMPING PARAM
-    public static final double JUMP_STRENGTH = 8.7;
-    public static final double DOUBLEJUMP_STRENGTH = JUMP_STRENGTH * 0.1;
+    public static final double JUMP_STRENGTH = 10;
+    public static final double DOUBLEJUMP_STRENGTH = JUMP_STRENGTH * 0.15;
     public static final double GRAVITY = -27;
     public static final double GRAVITY_AFTER_TIMEOUT = -100;
+    public static final int SINGLE_JUMP_CALORIE_COSTS = 500;
+    public static final int DOUBLE_JUMP_CALORIE_COSTS = 1000;
 
     //SNACK SPAWN
-    public static final int TIME_FOR_SNACKS_TO_RESPAWN = 30000;
-    public static final double SNACK_SPAWN_RATE = 0.5;
+    public static final int TIME_FOR_SNACKS_TO_RESPAWN = 10000;
+    public static final double SNACK_SPAWN_RATE = 0.1;
 
     //Chicken
     public static final String[] DEFAULT_CHICKEN_SCRIPTS = {"ChickenMovementSkript", "SonicChickenMovementSkript", "TalaChickenMovementSkript"};
@@ -32,7 +34,7 @@ public class GameConfig {
     public static final String GHOST_SCRIPT_EASY = "GhostMovementSkript";
     public static final String GHOST_SCRIPT_HARD = "SmartGhostMovementSkript";
     public static final double GHOST_RADIUS = 0.3;
-    public static final int GHOST_SPEED = 10;
+    public static final int GHOST_SPEED = 7;
     public static final int GHOST_DAMAGE = 300;
     public static final int AMOUNT_PLAYERS = 5;
     public static final String SCRIPT_GHOST_DIFFICULTY_MULTIPLAYER = "EASY";
